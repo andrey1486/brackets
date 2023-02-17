7823bcf059d31787e3af06146f497080d71c2930
Andrey <88458447+andrey1486@users.noreply.github.com>
2023-02-17 16:03:11 +0200

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let i, j, n, strTrans
+  i = str.length
+
+  while (i > 0) {
+    equalOneElementsStr = str.slice(i, (i + 1))
+    n = i
+    for (j = 0; j < bracketsConfig.length; j++) {
+      strTrans = (bracketsConfig[j][0]) + (bracketsConfig[j][1])
+      str = str.replace(strTrans, '')
+    }
+    i = str.length
+    if (n == i) { i = 0 }
+  }
+  
+  return !(str)
 }
